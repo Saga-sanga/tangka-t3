bd96e6c42f1abdcba7d584a3921ea50c8114760d
Saga-sanga <vulcan248@gmail.com>
2024-11-21 12:31:14 +0530

    feat: add indexes to the rest of the tables

--- src/server/db/schema.ts
+++ src/server/db/schema.ts
@@ -19,14 +19,22 @@ export const paymentEnum = pgEnum("paymentType", [
   "digital wallet",
 ]);
 
-export const organisations = pgTable("organisation", {
-  id: varchar("id", { length: 255 })
-    .notNull()
-    .primaryKey()
-    .$defaultFn(() => crypto.randomUUID()),
-  name: varchar("name", { length: 255 }).notNull(),
-  createdAt: timestamp("created_at").defaultNow().notNull(),
-});
+export const organisations = pgTable(
+  "organisation",
+  {
+    id: varchar("id", { length: 255 })
+      .notNull()
+      .primaryKey()
+      .$defaultFn(() => crypto.randomUUID()),
+    name: varchar("name", { length: 255 }).notNull(),
+    createdAt: timestamp("created_at").defaultNow().notNull(),
+  },
+  (table) => {
+    return {
+      nameIdx: index("organisation_name_idx").on(table.name),
+    };
+  },
+);
 
 export const organisationRelations = relations(organisations, ({ many }) => ({
   users: many(users),
@@ -37,22 +45,30 @@ export const organisationRelations = relations(organisations, ({ many }) => ({
   payments: many(payments),
 }));
 
-export const users = pgTable("user", {
-  id: varchar("id", { length: 255 })
-    .notNull()
-    .primaryKey()
-    .$defaultFn(() => crypto.randomUUID()),
-  organisationId: varchar("organisation_id", { length: 255 }).notNull(),
-  name: varchar("name", { length: 255 }),
-  email: varchar("email", { length: 255 }).notNull(),
-  emailVerified: timestamp("email_verified", {
-    mode: "date",
-    withTimezone: true,
-  }).default(sql`CURRENT_TIMESTAMP`),
-  image: varchar("image", { length: 255 }),
-  role: roleEnum("role"),
-  createdAt: timestamp("created_at").notNull().defaultNow(),
-});
+export const users = pgTable(
+  "user",
+  {
+    id: varchar("id", { length: 255 })
+      .notNull()
+      .primaryKey()
+      .$defaultFn(() => crypto.randomUUID()),
+    organisationId: varchar("organisation_id", { length: 255 }).notNull(),
+    name: varchar("name", { length: 255 }),
+    email: varchar("email", { length: 255 }).notNull(),
+    emailVerified: timestamp("email_verified", {
+      mode: "date",
+      withTimezone: true,
+    }).default(sql`CURRENT_TIMESTAMP`),
+    image: varchar("image", { length: 255 }),
+    role: roleEnum("role"),
+    createdAt: timestamp("created_at").notNull().defaultNow(),
+  },
+  (table) => {
+    return {
+      nameIdx: index("name_idx").on(table.name),
+    };
+  },
+);
 
 export const usersRelations = relations(users, ({ one, many }) => ({
   accounts: many(accounts),
@@ -150,7 +166,10 @@ export const items = pgTable(
   },
   (table) => {
     return {
-      nameIdx: index("name_idx").on(table.name),
+      nameCompositeIdx: index("idx_name_organisation_id").on(
+        table.organsationId,
+        table.name,
+      ),
     };
   },
 );
@@ -180,6 +199,9 @@ export const inventories = pgTable(
   (table) => {
     return {
       itemIdIdx: index("inventory_item_id_idx").on(table.itemId),
+      organisationIdIdx: index("items_organisation_id_idx").on(
+        table.organsationId,
+      ),
     };
   },
 );
@@ -192,16 +214,28 @@ export const inventoriesRelations = relations(inventories, ({ one, many }) => ({
   items: many(items),
 }));
 
-export const sales = pgTable("sales", {
-  id: varchar("id", { length: 255 })
-    .notNull()
-    .primaryKey()
-    .$defaultFn(() => crypto.randomUUID()),
-  userId: varchar("user_id", { length: 255 }).notNull(),
-  organsationId: varchar("organisation_id", { length: 255 }).notNull(),
-  totalAmount: decimal("total_amount", { precision: 10, scale: 2 }),
-  createdAt: timestamp("created_at").notNull().defaultNow(),
-});
+export const sales = pgTable(
+  "sales",
+  {
+    id: varchar("id", { length: 255 })
+      .notNull()
+      .primaryKey()
+      .$defaultFn(() => crypto.randomUUID()),
+    userId: varchar("user_id", { length: 255 }).notNull(),
+    organsationId: varchar("organisation_id", { length: 255 }).notNull(),
+    totalAmount: decimal("total_amount", { precision: 10, scale: 2 }),
+    createdAt: timestamp("created_at").notNull().defaultNow(),
+  },
+  (table) => {
+    return {
+      idxSalesOrgDate: index("idx_sales_org_date").on(
+        table.organsationId,
+        table.createdAt,
+      ),
+      salesUserId: index("sales_user_id").on(table.userId),
+    };
+  },
+);
 
 export const salesRelations = relations(sales, ({ one, many }) => ({
   users: one(users, {
@@ -216,18 +250,30 @@ export const salesRelations = relations(sales, ({ one, many }) => ({
   payments: many(payments),
 }));
 
-export const salesItems = pgTable("sales_items", {
-  id: varchar("id", { length: 255 })
-    .notNull()
-    .primaryKey()
-    .$defaultFn(() => crypto.randomUUID()),
-  salesId: varchar("sales_id", { length: 255 }).notNull(),
-  organisationId: varchar("organisation_id", { length: 255 }).notNull(),
-  itemId: varchar("item_id", { length: 255 }).notNull(),
-  quantity: integer("quantity").notNull(),
-  price: decimal("price", { precision: 10, scale: 2 }),
-  createdAt: timestamp("created_at").notNull().defaultNow(),
-});
+export const salesItems = pgTable(
+  "sales_items",
+  {
+    id: varchar("id", { length: 255 })
+      .notNull()
+      .primaryKey()
+      .$defaultFn(() => crypto.randomUUID()),
+    salesId: varchar("sales_id", { length: 255 }).notNull(),
+    organisationId: varchar("organisation_id", { length: 255 }).notNull(),
+    itemId: varchar("item_id", { length: 255 }).notNull(),
+    quantity: integer("quantity").notNull(),
+    price: decimal("price", { precision: 10, scale: 2 }),
+    createdAt: timestamp("created_at").notNull().defaultNow(),
+  },
+  (table) => {
+    return {
+      idxSalesItemsComposite: index("idx_sales_items_composite").on(
+        table.organisationId,
+        table.salesId,
+        table.itemId,
+      ),
+    };
+  },
+);
 
 export const salesItemsRelations = relations(salesItems, ({ one }) => ({
   sales: one(sales, {
@@ -244,17 +290,29 @@ export const salesItemsRelations = relations(salesItems, ({ one }) => ({
   }),
 }));
 
-export const payments = pgTable("payment", {
-  id: varchar("id", { length: 255 })
-    .notNull()
-    .primaryKey()
-    .$defaultFn(() => crypto.randomUUID()),
-  saleId: varchar("sale_id", { length: 255 }).notNull(),
-  organisationId: varchar("organisation_id", { length: 255 }).notNull(),
-  paymentMethod: paymentEnum("payment_method"),
-  amount: decimal("amount", { precision: 10, scale: 2 }),
-  createdAt: timestamp("created_at").notNull().defaultNow(),
-});
+export const payments = pgTable(
+  "payment",
+  {
+    id: varchar("id", { length: 255 })
+      .notNull()
+      .primaryKey()
+      .$defaultFn(() => crypto.randomUUID()),
+    saleId: varchar("sale_id", { length: 255 }).notNull(),
+    organisationId: varchar("organisation_id", { length: 255 }).notNull(),
+    paymentMethod: paymentEnum("payment_method"),
+    amount: decimal("amount", { precision: 10, scale: 2 }),
+    createdAt: timestamp("created_at").notNull().defaultNow(),
+  },
+  (table) => {
+    return {
+      idxPaymentComposite: index("idx_payment_composite").on(
+        table.organisationId,
+        table.saleId,
+        table.createdAt,
+      ),
+    };
+  },
+);
 
 export const paymentsRelations = relations(payments, ({ one }) => ({
   sale: one(sales, {
