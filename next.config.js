32f8df2e27c30d4908ecaa4968489237ffedbbe0
Saga-sanga <vulcan248@gmail.com>
2024-11-24 17:49:14 +0530

    feat: update docker for hmr

--- next.config.js
+++ next.config.js
@@ -6,6 +6,13 @@ import "./src/env.js";
 
 /** @type {import("next").NextConfig} */
 const config = {
+  // webpack: (config, { isServer }) => {
+  //   config.watchOptions = {
+  //     poll: 1000,
+  //     aggregateTimeout: 300,
+  //   }
+  //   return config
+  // },
   output: "standalone"
 };
 
--- src/server/api/routers/post.ts
+++ src/server/api/routers/post.ts
@@ -5,7 +5,7 @@ import {
   protectedProcedure,
   publicProcedure,
 } from "~/server/api/trpc";
-import { posts } from "~/server/db/schema";
+// import { items } from "~/server/db/schema";
 
 export const postRouter = createTRPCRouter({
   hello: publicProcedure
@@ -16,18 +16,18 @@ export const postRouter = createTRPCRouter({
       };
     }),
 
-  create: protectedProcedure
-    .input(z.object({ name: z.string().min(1) }))
-    .mutation(async ({ ctx, input }) => {
-      await ctx.db.insert(posts).values({
-        name: input.name,
-        createdById: ctx.session.user.id,
-      });
-    }),
+  // create: protectedProcedure
+  //   .input(z.object({ name: z.string().min(1) }))
+  //   .mutation(async ({ ctx, input }) => {
+  //     await ctx.db.insert(items).values({
+  //       name: input.name,
+  //       organsiationId: ctx.session.user.id
+  //     });
+  //   }),
 
   getLatest: protectedProcedure.query(async ({ ctx }) => {
-    const post = await ctx.db.query.posts.findFirst({
-      orderBy: (posts, { desc }) => [desc(posts.createdAt)],
+    const post = await ctx.db.query.items.findFirst({
+      orderBy: (items, { desc }) => [desc(items.createdAt)],
     });
 
     return post ?? null;
